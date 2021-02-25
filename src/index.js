4bd14177298241c1406c1f2d1d722a44df38dce2
frostwOw13 <frostwow13@yandex.ru>
2021-02-25 19:28:35 +0300

    feat: finish the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,64 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (num) {
+    let words = {
+        0: 'zero',
+        1: 'one',
+        2: 'two',
+        3: 'three',
+        4: 'four',
+        5: 'five',
+        6: 'six',
+        7: 'seven',
+        8: 'eight',
+        9: 'nine',
+        10: 'ten',
+        11: 'eleven',
+        12: 'twelve',
+        13: 'thirteen',
+        14: 'fourteen',
+        15: 'fifteen',
+        16: 'sixteen',
+        17: 'seventeen',
+        18: 'eighteen',
+        19: 'nineteen',
+        20: 'twenty',
+        30: 'thirty',
+        40: 'forty',
+        50: 'fifty',
+        60: 'sixty',
+        70: 'seventy',
+        80: 'eighty',
+        90: 'ninety'
 }
+
+let finalStr = [];
+
+if (num < 21) {
+    finalStr.push(words[num])
+    return finalStr.join(' ')
+} else if (num > 20 && num < 100) {
+    let [digit, simple] = [...String(num)]
+    finalStr.push(words[Number(digit + '0')])
+    if (simple != 0) {
+        finalStr.push(words[Number(simple)])
+    }
+    return finalStr.join(' ')
+} else if (num > 99 && num < 1000) {
+    let [hundred, digit, simple] = [...String(num)]
+    finalStr.push(`${words[hundred]} hundred`)
+    if (digit != 0) {
+        
+        if (digit == 1) {
+            finalStr.push(words[Number(String(digit) + String(simple))])
+            console.log(digit)
+            console.log(Number(String(digit) + String(simple)))
+            return finalStr.join(' ')
+        } else {
+            finalStr.push(words[Number(digit + '0')])
+        }
+    }
+    if (simple != 0) {
+        finalStr.push(words[Number(simple)])
+    } 
+    return finalStr.join(' ')
+}
+};
